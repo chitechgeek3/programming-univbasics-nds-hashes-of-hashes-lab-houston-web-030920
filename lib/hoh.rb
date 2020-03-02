25e3045f7afb02f68e51548abaadfad300b7e8a7
Rob Lawrence <iamrobnyce@gmail.com>
2020-03-02 05:26:24 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,45 +1,59 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+# Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
-def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
+DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+KATE_G = { name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
+def assembled_aoh
+  # Build an array that contains (or, "nests") the constants into a single
+  # Array. Ruby constants are denoted by ALL_CAPS
+  
+  constant_array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  
+  return constant_array
+  
+  
+end
+
+ #------------------------------------------
+ 
+def literal_aoh
+  # Using Array literal syntax only, build a nested array that uses the data
+  # held in the constants
+  
+  constant_array = [
+    
+    {:name=>"Don Gately", :occupation=>"Live-in Staff"}, 
+    {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}, 
+    {:name=>"Pat Monteseian", :occupation=>"Staff"},
+    {:name=>"Kate Gompert", :occupation=>"None"}, 
+    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+    
+    ]
+    
+  return constant_array
+end
+
+ #------------------------------------------
+ 
+def aoh_lookup(aoh, row, key)
+  
+  return aoh[row][key]
+  
+end
+
+#------------------------------------------
+ 
+def aoh_update(aoh, row, key, new_value)
+  # Update the AoH data at row and key to have the value of new_value
+  # Return the updated AoH
+  
+  
+  aoh[row][key] = new_value
+  
+  return aoh 
+  
+  
 end
